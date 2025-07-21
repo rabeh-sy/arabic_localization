7ca5a6eb7b4efdd35d26b1d89fdc87a54d63f6e0
khaled-badenjki <m.k.badenjki2@gmail.com>
2025-07-21 20:58:54 +0300

    Revert "add patches to update doctype"
    This reverts commit 1c2b36295969c2cf71be38e7287f956fe3bb0bdf.

--- arabic_localization/patches/v0_1/add_plural_name_fields.py
+++
@@ -1,8 +0,0 @@
-import frappe
-
-def execute():
-    # Add plural_name field to DocType if it doesn't exist
-    frappe.db.sql_ddl("""
-        ALTER TABLE `tabDocType`
-        ADD COLUMN IF NOT EXISTS `plural_name` VARCHAR(255)
-    """)
